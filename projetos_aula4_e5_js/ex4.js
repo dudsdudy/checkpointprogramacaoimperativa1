79a1914d79a35d3ceaf10dc25e35c8e5e7baa618
TiagoS <tiago-de-santana@hotmail.com>
2022-05-30 12:26:05 -0300

    Exercicio 4 Calculando o IMC

---
+++ projetos_aula4_e5_js/ex4.js
@@ -0,0 +1,9 @@
+const nomeCompleto = "Tiago de Santana Batista" ;
+let idade = "29";
+let peso = "130";
+let altura = "1.81";
+let plano = "True";
+var imc = Math.pow(altura,altura) 
+let formularIMC = peso / imc;
+
+console.log (nomeCompleto + " " + "tem" + " " + idade + " " + "e seu indíce de massa corporal é " + formularIMC)
