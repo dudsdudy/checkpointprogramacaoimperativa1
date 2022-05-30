a050d5cf98e9b7479e8b303a7b41217a41db2737
TiagoS <tiago-de-santana@hotmail.com>
2022-05-30 14:49:29 -0300

    primeira solução em seguros

---
+++ projetos_aula4_e5_js/solucao_vida.js
@@ -0,0 +1,13 @@
+{
+  let nomeCompleto = "Tiago de Santana Batista";
+  let despesaMensal = "2500";
+  let capitalIdeal = despesaMensal * 60;
+
+  console.log(
+    "Olá " +
+      nomeCompleto + " " +
+      "sua cobertura ideal para o seguro de vida é " +
+      "R$" +
+      capitalIdeal
+  );
+}
